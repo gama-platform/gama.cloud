012a89aa1031bea269c80e8cb809eec86e11d239
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-07-27 21:20:05 +0700

    test travis, sorry for the spam

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd msi.gama.lang.gaml.web.build && 
-mvn clean deploy:deploy-file -Dfile=/home/travis/build/gama-platform/gama.cloud/msi.gama.lang.gaml.web.build/GamaWeb/target/GamaWeb.war 
--Durl=scp://51.255.46.42/./var/www/gama_cloud -P p2Repo --settings ../settings.xml -Dmaven.test.skip=true && 
+mvn clean deploy -P p2Repo --settings ../settings.xml -Dmaven.test.skip=true && 
 cd -
 
 
