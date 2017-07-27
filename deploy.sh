cb9a910d073a68c4330260ba7f56b1679cfd46e3
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-07-27 22:12:46 +0700

    test travis, sorry for the spam

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd msi.gama.lang.gaml.web.build && 
-mvn clean deploy -P p2Repo --settings ../settings.xml -Dmaven.test.skip=true && 
+mvn clean install && 
+mvn deploy -P p2Repo --settings ../settings.xml -Dmaven.test.skip=true && 
 cd -
 
 
