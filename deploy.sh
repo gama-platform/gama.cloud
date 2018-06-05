b5dc4244126dee1398872c5d31abd569744cd54a
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-06-06 00:08:16 +0700

    try fix touch event on chrom mobile

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd msi.gama.lang.gaml.web.build && 
-mvn clean deploy -P p2Repo --settings ../settings.xml -Dmaven.test.skip=true && 
+mvn clean install && 
+cd GamaWeb && 
+mvn deploy -P p2Repo --settings ../settings.xml -Dmaven.test.skip=true && 
 cd -
 
 
