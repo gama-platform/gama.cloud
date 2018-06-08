f23c9d552bbbe0214139a394699f1505d6d95437
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-06-08 07:32:56 -0700

    remove unused dsl plugins

--- org.dslforge.xtext.generator.ui/src/org/dslforge/xtext/generator/ui/wizard/IXtextGrammarConstants.java
+++
@@ -1,7 +0,0 @@
-package org.dslforge.xtext.generator.ui.wizard;
-
-public interface IXtextGrammarConstants {
-
-	public static final String XTEXT_FILE_EXTENSION = "xtext";
-	public static final String XTEXT_FILE_EXTENSION_FILTER = "*.xtext";
-}
