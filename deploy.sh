2b4be5f929bf0c10c83fdaaa8fc3142da896e944
HUYNH Quang Nghi <hqnghi88@gmail.com>
2019-03-19 14:56:11 -0700

    add business theme ci deploy ci release

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd msi.gama.lang.gaml.web.build &&
 cd GamaWeb && 
 mvn deploy -P p2Repo --settings ../../settings.xml -Dmaven.test.skip=true && 
 cd ../.. &&
-cd gama.tomcat && 
+cd cict.gama.tomcat && 
 mvn clean install && 
 cd -
 
