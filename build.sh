5e8e0dcb321d107b080f4c954ec76e2e9a60ebeb
HUYNH Quang Nghi <hqnghi88@gmail.com>
2019-03-25 11:18:52 +0700

    remove wrong package org.eclipse.swt (not from rwt) in navigator.web ci release ci deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-cd msi.gama.lang.gaml.web.build &&
+#!/bin/bash
+cd msi.gama.lang.gaml.web.build -X &&
 mvn clean install &&
-cd - 
+cd GamaWeb && 
+mvn clean install && 
+cd ../.. &&
+cd cict.gama.tomcat && 
+mvn clean install && 
+cd - &&
+cp cict.gama.tomcat/target/tomcat_launcher.jar cict.gama.tomcat/target/GamaWeb/tomcat_launcher.jar
+
