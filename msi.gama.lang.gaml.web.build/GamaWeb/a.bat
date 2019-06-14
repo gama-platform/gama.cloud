9a9195311fdd57aea742683c0039427547d00c88
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-06-14 21:08:21 +0700

    test

--- msi.gama.lang.gaml.web.build/GamaWeb/a.bat
+++ msi.gama.lang.gaml.web.build/GamaWeb/a.bat
@@ -1,5 +1,5 @@
 net stop Tomcat9
 del "C:\Program Files\Apache Software Foundation\Tomcat 9.0\logs\*.*" /F /Q
 del "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\GamaWeb.war" /F
-copy "c:\git\gama.cloud\msi.gama.lang.gaml.web.build\GamaWeb\target\GamaWeb.war" "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps"
+copy "e:\git\gama.cloud\msi.gama.lang.gaml.web.build\GamaWeb\target\GamaWeb.war" "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps"
 net start Tomcat9
