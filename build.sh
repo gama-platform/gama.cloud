f25c42391ebc979eae710b87e8831eb9b03a9618
HUYNH Nghi <hqnghi88@gmail.com>
2020-03-23 18:51:21 +0700

    ci release

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
+b(){
+cd msi.gama.lang.gaml.web.build/GamaWeb 
+mvn clean install 
+cd ../../cict.gama.tomcat
+mvn clean install
+cd target
+cp tomcat_launcher.jar GamaWeb
+cd ../..
+}
+a(){
+rm -rf ummisco.gama.opengl.web/gaml
+rm -rf ummisco.gama.java2d.web/gaml
 cd msi.gama.lang.gaml.web.target
 mvn clean install
 cd ../msi.gama.lang.gaml.web.build
 mvn clean install
+res=$?
+if [[ $res -ne 0 ]]; then
+	exit $res
+fi
 cd GamaWeb 
 mvn clean install 
 cd ../../cict.gama.tomcat
@@ -9,3 +25,5 @@ mvn clean install
 cd target
 cp tomcat_launcher.jar GamaWeb
 cd ../..
+}
+b
--- cict.gama.tomcat/script/gama.bat
+++ cict.gama.tomcat/script/gama.bat
@@ -1,4 +1,1 @@
 start java -jar .\tomcat_launcher.jar
-@echo "Wait 10s to startup"
-timeout 10 /nobreak
-start "" http://localhost:8080/GamaWeb/texteditor
